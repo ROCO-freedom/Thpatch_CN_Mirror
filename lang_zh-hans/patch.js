d2f49184abf66153e578339fd5a0314b509ea824
ROCO2017 <1401548627@qq.com>
2021-12-27 19:03:04 +0800

    修复仓库标识错误。

--- lang_zh-hans/patch.js
+++ lang_zh-hans/patch.js
@@ -5,7 +5,7 @@
     ],
     "id": "lang_zh-hans",
     "servers": [
-        "hhttps://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/main/lang_zh-hans/"
+        "https://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/main/lang_zh-hans/"
     ],
     "title": "Simplified Chinese language pack",
     "update": true

7a5f9c010ab519021efbecc4fd17b2d45a46c664
ROCO2017 <1401548627@qq.com>
2021-12-27 18:23:10 +0800

    添加标识信息。

---
+++ repo.js
@@ -0,0 +1,14 @@
+{
+	"contact": "rocozxc@gmail.com",
+	"id": "thCNPMirror",
+	"patches": {
+		"aero": "Enable Aero compositing",
+		"base_tasofro": "Basic support for Tasogare Frontier games",
+		"base_tsa": "Basic support for Team Shanghai Alice games",
+		"lang_zh-hans": "Simplified Chinese language pack"
+	},
+	"servers": [
+		"https://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/master/"
+	],
+	"title": "Thpatch Simplified Chinese(lang_zh-hans) branch's Mirror."
+}

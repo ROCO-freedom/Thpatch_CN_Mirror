a3804a277973f0f3778c7f47a37361d80153a6e9
ROCO2021 <1401548627@qq.com>
2021-12-27 14:15:59 +0000

    定时补丁文件更新。2021-12-27===>14:15(UTC)

--- base_tasofro/patch.js
+++ base_tasofro/patch.js
@@ -1,8 +1,8 @@
 {
-  "id": "base_tasofro",
-  "servers": [
-    "https://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/main/base_tasofro/"
-  ],
-  "thcrap_version_min": "0x20191214",
-  "title": "Basic support for Tasogare Frontier games"
+	"id": "base_tasofro",
+	"servers": [
+		"https://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/main/base_tasofro/"
+	],
+	"thcrap_version_min": "0x20191214",
+	"title": "Basic support for Tasogare Frontier games"
 }
--- lang_zh-hans/patch.js
+++ lang_zh-hans/patch.js
@@ -1,12 +1,12 @@
 {
-    "dependencies": [
-        "nmlgc/base_tsa",
-        "nmlgc/base_tasofro"
-    ],
-    "id": "lang_zh-hans",
-    "servers": [
-        "https://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/main/lang_zh-hans/"
-    ],
-    "title": "Simplified Chinese language pack",
-    "update": true
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "lang_zh-hans",
+	"servers": [
+		"https://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/main/lang_zh-hans/"
+	],
+	"title": "Simplified Chinese language pack",
+	"update": true
 }

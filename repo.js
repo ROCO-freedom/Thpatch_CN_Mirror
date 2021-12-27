cdb5e8ba9832d3651de28701b9cd1e6e2859acaf
ROCO2017 <1401548627@qq.com>
2021-12-27 19:06:01 +0800

    修复仓库标识错误。

--- repo.js
+++ repo.js
@@ -8,7 +8,7 @@
 		"lang_zh-hans": "Simplified Chinese language pack"
 	},
 	"servers": [
-		"https://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/master/"
+		"https://thpatch-cn.coding.net/p/thcrapcn/d/Thpatch_CN_Patchfiles_Mirror/git/raw/main/"
 	],
 	"title": "Thpatch Simplified Chinese(lang_zh-hans) branch's Mirror."
 }

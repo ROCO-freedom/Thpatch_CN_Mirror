16bfb5fdbcd8d7eb6c5e55be855bc916f2e7ae7a
ROCO2021 <1401548627@qq.com>
2021-12-28 16:07:04 +0800

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,10 +1,6 @@
 {
 	"contact": "rocozxc@gmail.com",
 	"id": "thCNPMirror",
-	"neighbors": [
-		"https://thpatch-cn.coding.net/p/thcrapcn/d/thcrap-tsa-CN-mirror/git/raw/master/",
-		"https://thpatch-cn.coding.net/p/thcrapcn/d/tpZHCNex/git/raw/master/",
-	],
 	"patches": {
 		"lang_zh-hans": "Simplified Chinese language pack"
 	},
